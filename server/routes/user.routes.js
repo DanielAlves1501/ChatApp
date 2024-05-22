25dfd124cb495478e3f154266886b5536ffbef31
Daniel Alves <danielgsmax@gmail.com>
2024-05-22 13:49:49 -0400

    second commit

--- server/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protectRoute from "../middlewares/protectRoute.js";
-import { getUsersForSideBar } from "../controllers/user.controller.js";
-
-const router = express.Router();
-
-router.get("/", protectRoute, getUsersForSideBar);
-
-export default router;
